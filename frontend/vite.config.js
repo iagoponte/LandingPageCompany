9016393556a72695b89481d853b0678118c4da81
iagoponte <iago.ponte.01@gmail.com>
2025-05-14 10:22:43 -0300

    fix: merging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 02a881eadff78309a66bee9f6d61dfc3a68edabf
 })
