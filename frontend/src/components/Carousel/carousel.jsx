a984a66d481d622525905b7e9906331e5a346c3c
iagoponte <iago.ponte.01@gmail.com>
2025-05-21 08:57:18 -0300

    fix: carousel

--- frontend/src/components/Carousel/carousel.jsx
+++ frontend/src/components/Carousel/carousel.jsx
@@ -1,34 +1,86 @@
-import useEmblaCarousel from 'embla-carousel-react';
-import EolicaImgOne from '../../assets/images/eolicas01.webp';
-import EolicaImgTwo from '../../assets/images/eolicas02.webp';
-import EolicaImgThree from '../../assets/images/eolicas03.webp';
-import EolicaImgFour from '../../assets/images/eolicas04.webp';
+import useEmblaCarousel from "embla-carousel-react";
+import { useCallback } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import EolicaImgOne from "../../assets/images/eolicas01.webp";
+import EolicaImgTwo from "../../assets/images/eolicas02.webp";
+import EolicaImgThree from "../../assets/images/eolicas03.webp";
+import EolicaImgFour from "../../assets/images/eolicas04.webp";
 
 export const Carousel = () => {
-    const [emblaRef] = useEmblaCarousel({
-        loop: true,
-        speed: 10,
-        dragFree: true,
-        containScroll: 'trimSnaps',
-        skipSnaps: false,
-    });
-    
-    return (
-    <div className="embla overflow-hidden rounded-xl shadow-lg max-w-3xl mx-auto" ref={emblaRef}>
-      <div className="embla__container flex">
-        <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4"> 
-            <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgOne} alt='torres eólicas'></img>
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    speed: 2,
+    dragFree: true,
+    containScroll: "trimSnaps",
+    skipSnaps: false,
+  });
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  // Navegação: avançar
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    // <div className="embla overflow-hidden rounded-xl shadow-lg max-w-3xl mx-auto" ref={emblaRef}>
+    //   <div className="embla__container flex">
+    //     <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
+    //         <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgOne} alt='torres eólicas'></img>
+    //     </div>
+    //     <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
+    //         <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgTwo} alt='torres eólicas'></img>
+    //     </div>
+    //     <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
+    //         <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgThree} alt='torres eólicas'></img>
+    //     </div>
+    //     <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
+    //         <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgFour} alt='torres eólicas'></img>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="relative max-w-3xl mx-auto">
+      {/* Carrossel */}
+      <div
+        className="embla overflow-hidden rounded-xl"
+        ref={emblaRef}
+      >
+        <div className="embla__container flex mb-0">
+          {[EolicaImgOne, EolicaImgTwo, EolicaImgThree, EolicaImgFour].map(
+            (img, index) => (
+              <div
+                key={index}
+                className="embla__slide min-w-full flex-shrink-0 backdrop-blur-md p-2"
+              >
+                <img
+                  className="w-full h-72 rounded-2xl object-cover"
+                  src={img}
+                  alt={`torres eólicas ${index + 1}`}
+                />
+              </div>
+            )
+          )}
         </div>
-        <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
-            <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgTwo} alt='torres eólicas'></img>
-        </div>
-        <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4">
-            <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgThree} alt='torres eólicas'></img>
-        </div>
-        <div className="embla__slide min-w-full flex-shrink-0 bg-white/30 backdrop-blur-md p-4"> 
-            <img className="w-full h-100 rounded-2x1 object-cover" src={EolicaImgFour} alt='torres eólicas'></img>
+        {/* Botões */}
+        <div className="flex justify-center space-x-4">
+          <button
+        onClick={scrollPrev}
+        className="px-4 py-0 mb-2 rounded-full transition"
+      >
+        <ArrowLeft className="hover:py-1"/>
+      </button>
+
+      <button
+        onClick={scrollNext}
+        className="px-4 py-0 mb-2 rounded-full transition"
+      >
+        <ArrowRight className="hover:py-1"/>
+      </button>
         </div>
       </div>
     </div>
-    )
-    }
+  );
+};
--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -6,22 +6,36 @@ import { HeroSection } from "../../layout/HeroSection/heroSection";
 export const Home = () => {
   return (
     <>
-        <HeroSection />
-        <div>
-          <Carousel />
-          <h2>
+      <HeroSection />
+      <div className="flex flex-row items-start justify-start gap-4 px-4">
+        <div className="w-1/2 py-4" >
+          <Carousel/>
+        </div>
+        <div className="w-1/2 ml-0 py-4">
+          <h2 className="text-md font-bold">Informaçõe dos serviços</h2>
+          <h3>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
             error totam aspernatur dolores tenetur, repudiandae nemo
             reprehenderit reiciendis fuga minus delectus harum nesciunt velit
-            debitis dicta ex expedita alias facere!
-          </h2>
-          <h3>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi quia
-            quam repellendus, itaque sapiente quas nostrum ducimus eligendi
-            quisquam doloribus illum modi corporis autem ipsa reprehenderit
-            assumenda voluptatum dolor dolorum.
+            debitis dicta ex expedita alias facere!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Minima error totam aspernatur dolores
+            tenetur, repudiandae nemo reprehenderit reiciendis fuga minus
+            delectus harum nesciunt velit debitis dicta ex expedita alias
+            facere!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Minima error totam aspernatur dolores tenetur, repudiandae nemo
+            reprehenderit reiciendis fuga minus delectus harum nesciunt velit
+            debitis dicta ex expedita alias facere!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Minima error totam aspernatur dolores
+            tenetur, repudiandae nemo reprehenderit reiciendis fuga minus
+            delectus harum nesciunt velit debitis dicta ex expedita alias
+            facere!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Minima error totam aspernatur dolores tenetur, repudiandae nemo
+            reprehenderit reiciendis fuga minus delectus harum nesciunt velit
+            debitis dicta ex expedita alias facere!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. 
           </h3>
         </div>
+      </div>
     </>
   );
 };
